93f325e9e447ecacfdb5f599f046355c77aacdea
zsw <546232194@qq.com>
2021-06-24 10:06:41 +0800

    策略模式消除复杂if else

--- src/main/java/com/line/util/JsonConvertUtils.java
+++ src/main/java/com/line/util/JsonConvertUtils.java
@@ -27,7 +27,7 @@ public class JsonConvertUtils {
         list.add(1);
         System.out.println(list.size());
         list.stream()
-                .collect(Collectors.toMap(e -> e, e -> 1, (a, b) -> a + b)) // 获得元素出现频率的 Map，键为元素，值为元素出现的次数
+                .collect(Collectors.toMap(e -> e, e -> 1, Integer::sum)) // 获得元素出现频率的 Map，键为元素，值为元素出现的次数
                 .entrySet().stream() // Set<Entry>转换为Stream<Entry>
                 .filter(entry -> entry.getValue() > 1) // 过滤出元素出现次数大于 1 的 entry
                 .map(entry -> entry.getKey()) // 获得 entry 的键（重复元素）对应的 Stream
