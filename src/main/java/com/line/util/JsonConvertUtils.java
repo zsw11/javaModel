deadf5c84b7ef5daec3593798bd3394d0cbed2c1
shuwen.zhou <546232194@qq.com>
2021-09-15 10:59:16 +0800

    1

--- src/main/java/com/line/util/JsonConvertUtils.java
+++ src/main/java/com/line/util/JsonConvertUtils.java
@@ -26,12 +26,26 @@ public class JsonConvertUtils {
         list.add(2);
         list.add(1);
         System.out.println(list.size());
-        list.stream()
-                .collect(Collectors.toMap(e -> e, e -> 1, Integer::sum)) // 获得元素出现频率的 Map，键为元素，值为元素出现的次数
-                .entrySet().stream() // Set<Entry>转换为Stream<Entry>
-                .filter(entry -> entry.getValue() > 1) // 过滤出元素出现次数大于 1 的 entry
-                .map(entry -> entry.getKey()) // 获得 entry 的键（重复元素）对应的 Stream
-                .collect(Collectors.toList()); // 转化为 List
-        System.out.println(list);
+//        list.stream()
+//                .collect(Collectors.toMap(e -> e, e -> 1, Integer::sum)) // 获得元素出现频率的 Map，键为元素，值为元素出现的次数
+//                .entrySet().stream() // Set<Entry>转换为Stream<Entry>
+//                .filter(entry -> entry.getValue() > 1) // 过滤出元素出现次数大于 1 的 entry
+//                .map(entry -> entry.getKey()) // 获得 entry 的键（重复元素）对应的 Stream
+//                .collect(Collectors.toList()); // 转化为 List
+//        System.out.println(list);
+        for (int a = 0; a < list.size(); a++) {
+            final int finalA = a; // final 修饰的变量指向的地址不能变
+            List<Integer> collect = list.stream().filter(integer -> list.get(finalA).equals(1)).collect(Collectors.toList());
+            System.out.println(collect);
+        }
+        String s = "s";
+        final String finals = s;
+        s = "s1";
+        System.out.println(s);
+        int a = 1;
+        final int finalInt = a;
+        a = 3;
+        System.out.println(a);
+
     }
 }
