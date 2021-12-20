b921d12ef428b0f5fe38252be4b5b7f5b31bf25c
shuwen.zhou <546232194@qq.com>
2021-12-20 09:54:21 +0800

    SynchronousQueueDemo

--- src/main/java/com/line/App.java
+++ src/main/java/com/line/App.java
@@ -1,5 +1,13 @@
 package com.line;
 
+import com.sun.corba.se.impl.protocol.giopmsgheaders.LocateReplyMessage_1_0;
+import sun.security.krb5.internal.crypto.Aes128;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +16,37 @@ public class App
 {
     public static void main( String[] args )
     {
+//        addEffect();
+//        long[] arr = new long[]{1,4,3,3};
+//        List list = Arrays.asList(arr);//基本类型不支持泛型化，会把整个数组当成一个元素放入新的数组，传入可变参数
+//        System.out.println(list.size());//打印1
         System.out.println( "Hello World!" );
+
+        long[] arr = new long[]{1l,4l,3l,3l};
+        List list = Arrays.asList(arr);
+        System.out.println(list.get(0));
+    }
+    public static void addEffect(){
+        //不指定下标插入
+        int length = 10000000;
+        ArrayList<Integer> al = new ArrayList<>();//指定容量时   效率相当
+        LinkedList<Integer> ll = new LinkedList<>();
+
+        for(int i=0;i <length;i++){
+            ll.add(i);
+        }
+
+        long start6 = System.currentTimeMillis();
+        ll.add(8888);
+        long end6 = System.currentTimeMillis();
+        System.out.println(end6-start6);
+
+        for(int i=0;i <length;i++){
+            al.add(i);
+        }
+        long start5 = System.currentTimeMillis();
+        al.add(8888);
+        long end5 = System.currentTimeMillis();
+        System.out.println(end5-start5);
     }
 }
